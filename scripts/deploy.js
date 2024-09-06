18bc94e7bdaa5518cb14f5990df004405c217aa8
vaibhavvvvv <vaibhavng7@gmail.com>
2024-09-07 01:36:05 +0530

    contracts deployed locally

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,51 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
 async function main() {
+  //setup accounts
+  [buyer, seller, lender, inspector]   = await ethers.getSigners();  
+  //deploy Real Estate
+  const RealEstate= await ethers.getContractFactory('RealEstate');
+  realEstate =  await RealEstate.deploy();
+  await realEstate.deployed()
+
+  console.log(`Deployed Real Estate contract at ${realEstate.address}`);
+  console.log(`Minting 3 properties...`);
+
+  for(let i = 0; i<3; i++){
+    let transaction = await realEstate.connect(seller).mint(`https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/${i +1}.json`);
+    await transaction.wait();
+  }
+
+  //Deploy Escrow
+  const Escrow = await ethers.getContractFactory('Escrow');
+  escrow = await Escrow.deploy( realEstate.address, seller.address, lender.address, inspector.address );
+  await escrow.deployed();
+
+  console.log(`Deployed Escrow contract at ${escrow.address}`);
+
+  for(let i = 0; i<3; i++){
+    //Approve properties
+    let transaction = await realEstate.connect(seller).approve(escrow.address, i+1);
+    await transaction.wait()
+  }
+
+
+    //List properties
+    let transaction = await escrow.connect(seller).list(1, buyer.address, tokens(20), tokens(10))
+    await transaction.wait()
+
+    transaction = await escrow.connect(seller).list(2, buyer.address, tokens(15), tokens(5))
+    await transaction.wait()
+    
+    transaction = await escrow.connect(seller).list(3, buyer.address, tokens(10), tokens(5))
+    await transaction.wait()
+
+    console.log('...finished')
 
 }
 

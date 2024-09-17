0125cf05a9b7cc69a7fae4231d15cb9ca2e4d807
vaibhavvvvv <vaibhavng7@gmail.com>
2024-09-18 02:59:08 +0530

    buy properties implemented

--- src/App.js
+++ src/App.js
@@ -15,15 +15,95 @@ import config from './config.json';
 
 function App() {
 
+  const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [escrow, setEscrow] = useState(null);
+  const [homes, setHomes] = useState([]);
+  const [home, setHome] = useState({});
+  const [toggle, setToggle] = useState(false);
+
+  const loadBlockchainData = async() =>{
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+
+    const network =  await provider.getNetwork();
+    console.log("config[network.chainId].realEstate.address ", config[network.chainId].realEstate.address)
+    console.log("RealEstate", RealEstate)
+    console.log("providr", provider)
+
+    const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate.abi, provider);
+    console.log("realestate",realEstate)
+
+    const totalSupply = await realEstate.totalSupply();
+    console.log('totalsupply',totalSupply.toString())
+    const homes = [];
+
+    for(var i = 1; i<= totalSupply; i++){
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json()
+      homes.push(metadata);
+      console.log('homes', homes)
+    }
+
+    setHomes(homes);
+
+    const escrow = new ethers.Contract(config[network.chainId].escrow.address, Escrow, provider);
+    setEscrow(escrow);
+
+    window.ethereum.on('accountsChanged', async()=>{
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = ethers.utils.getAddress(accounts[0])
+      setAccount(account);
+    })
+   
+
+  }
+
+
+   useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
+  const toggleProp = (home) => {
+    setHome(home);
+    toggle ? setToggle(false): setToggle(true);
+  }
+
   return (
-    <div>
+    <div> 
 
+      <Navigation account={account} setAccount={setAccount}  />
+      <Search />
       <div className='cards__section'>
+        <h3>Homes for you</h3>
+        <hr />
+        
+        <div className='cards' >
+        {homes.map((home,index)=>(
+          <div className='card' key={index} onClick={() => toggleProp(home) } >
+            <div className='card__image' >
+              <img src = {home.image} alt='Home'/>
+            </div>
+            <div className='card__info' >
+              <h4>{home.attributes[0].value} ETH</h4>
+              <p>
+                <strong>{home.attributes[2].value}</strong> beds |
+                <strong>{home.attributes[3].value}</strong> baths |
+                <strong>{home.attributes[4].value}</strong> sqft |
+              </p>
+              <p>{home.address}</p>
+            </div>
+          </div>
 
-        <h3>Welcome to Millow</h3>
+        ))}
 
+        </div>
       </div>
 
+          {toggle  && (
+            <Home home={home} provider={provider} escrow={escrow} account={account} toggleProp={toggleProp} />
+          )}
     </div>
   );
 }
